836dc746c5f28b36f055ef6d72d38fb816c51ccf
Cesar Castillo <ccastill100@gmail.com>
2023-02-16 11:17:56 +0100

    Add DELETE operation for /mqtt endpoint

--- src/main/java/hvtask/ccb/controllers/MqttController.java
+++ src/main/java/hvtask/ccb/controllers/MqttController.java
@@ -3,9 +3,12 @@ package hvtask.ccb.controllers;
 import hvtask.ccb.models.Broker;
 import hvtask.ccb.models.BrokerPutRequest;
 import hvtask.ccb.storage.BrokerDao;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.*;
 import org.jdbi.v3.core.Jdbi;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Controller("/mqtt")
@@ -26,5 +29,15 @@ public class MqttController {
     @Get("/{brokername}")
     public Optional<Broker> getBroker(@PathVariable("brokername") String brokername) {
         return jdbi.withExtension(BrokerDao.class, dao -> dao.findByName(brokername));
+    }
+
+    @Delete("/{brokername}")
+    public HttpResponse<Map<String, String>> deleteByName(@PathVariable String brokername) {
+        int deleted = jdbi.withExtension(BrokerDao.class, dao -> dao.deleteByName(brokername));
+        if (deleted == 0) {
+            return HttpResponse.status(HttpStatus.valueOf(202))
+                    .body(Map.of("error", "Broker name " + brokername + " was not removed"));
+        }
+        return HttpResponse.status(HttpStatus.valueOf(204));
     }
 }
