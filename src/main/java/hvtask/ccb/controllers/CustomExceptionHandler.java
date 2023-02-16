c842d8c48b4a774c6bc251bff39ec2806349c71b
Cesar Castillo <ccastill100@gmail.com>
2023-02-16 11:53:42 +0100

    Fix global exception catcher to no hide exceptions from logger

--- src/main/java/hvtask/ccb/controllers/CustomExceptionHandler.java
+++ src/main/java/hvtask/ccb/controllers/CustomExceptionHandler.java
@@ -6,6 +6,8 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
@@ -13,9 +15,11 @@ import java.util.Map;
 @Singleton
 @Requires(classes = {MqttController.class, ExceptionHandler.class})
 public class CustomExceptionHandler implements ExceptionHandler<Exception, HttpResponse> {
+    private static final Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
 
     @Override
     public HttpResponse handle(HttpRequest request, Exception exception) {
+        logger.error("Top level catch: ", exception);
         return HttpResponse.serverError(Map.of("error","Something went wrong. We will look at it shortly."));
     }
 }
