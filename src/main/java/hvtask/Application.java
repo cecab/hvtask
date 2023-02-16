594777b0a3d4aace1e436afdc3a2ee679bbd2826
Cesar Castillo <ccastill100@gmail.com>
2023-02-16 10:19:17 +0100

    Model a broker object in storage layer

---
+++ src/main/java/hvtask/Application.java
@@ -0,0 +1,12 @@
+package hvtask;
+
+import io.micronaut.runtime.Micronaut;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+
+}
