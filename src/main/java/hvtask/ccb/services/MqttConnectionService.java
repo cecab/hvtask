e51f8e651c76ebbc393af015053acb61b9ad9534
Cesar Castillo <ccastill100@gmail.com>
2023-02-17 10:52:20 +0100

    Add subscription while keeping reusable client per broker

--- src/main/java/hvtask/ccb/services/MqttConnectionService.java
+++ src/main/java/hvtask/ccb/services/MqttConnectionService.java
@@ -5,6 +5,7 @@ import com.hivemq.client.mqtt.MqttClientState;
 import com.hivemq.client.mqtt.mqtt3.Mqtt3BlockingClient;
 import hvtask.ccb.models.Broker;
 import hvtask.ccb.storage.BrokerDao;
+import io.reactivex.rxjava3.core.Observable;
 import jakarta.inject.Singleton;
 import org.jdbi.v3.core.Jdbi;
 import org.slf4j.Logger;
@@ -19,35 +20,65 @@ import java.util.UUID;
 public class MqttConnectionService {
     private static final Logger logger = LoggerFactory.getLogger(MqttConnectionService.class);
     private final Jdbi jdbi;
-    private final Map<String, Mqtt3BlockingClient> publishersMap;
+    private final Map<String, Mqtt3BlockingClient> brokerNameToClientMap;
 
     public MqttConnectionService(Jdbi jdbi) {
         this.jdbi = jdbi;
-        publishersMap = new HashMap<>();
+        brokerNameToClientMap = new HashMap<>();
     }
 
     public Optional<Publisher> lookupPublisher(String brokerName) {
+        return checkBorkerInDBAndBuildAClient(brokerName)
+                .map(Publisher::new);
+    }
+
+    private Optional<Mqtt3BlockingClient> checkBorkerInDBAndBuildAClient(String brokerName) {
         return jdbi.withExtension(BrokerDao.class, dao -> dao.findByName(brokerName))
                 .flatMap(brokerObj -> {
                     // lookup in our local Map
                     String identifier = brokerObj.getName() + "-" + UUID.randomUUID();
-                    return Optional.ofNullable(publishersMap.get(brokerName))
+                    return Optional.ofNullable(brokerNameToClientMap.get(brokerName))
                             .map(localClient -> {
-                                // Check local client state. if they are not CONNECTED, they are discarded.
-                                if (localClient.getState() != (MqttClientState.CONNECTED)) {
-                                    return publishersMap.put(brokerName, createAndConnectClient(brokerObj, identifier));
+                                if (localClient.getState() == MqttClientState.CONNECTED) {
+                                    return localClient;
                                 }
-                                return localClient;
-                            }).or(() -> {
-                                // Build a new client from scratch
-                                return Optional.of(createAndConnectClient(brokerObj, identifier));
+                                return createAndConnectClient(brokerObj, identifier);
                             })
-                            .map(Publisher::new);
+                            .or(() -> {
+                                // Build a new client from scratch and add it to our map
+                                brokerNameToClientMap.put(brokerName, createAndConnectClient(brokerObj, identifier));
+                                return Optional.of(brokerNameToClientMap.get(brokerName));
+                            });
+                });
+    }
+
+    public Optional<Observable<String>> lookupSubscriber(
+            String brokerName,
+            String topic) {
+        return checkBorkerInDBAndBuildAClient(brokerName)
+                .map(client -> {
+                    var asyncClient = client.toAsync();
+                    return Observable.create(emitter -> {
+                        asyncClient.subscribeWith()
+                                .topicFilter(topic)
+                                .callback(publish -> {
+                                    emitter.onNext(new String(publish.getPayloadAsBytes()));
+                                })
+                                .send()
+                                .whenComplete((subAck, throwable1) -> {
+                                    if (throwable1 != null) {
+                                        logger.error(" Failed in my subscription attempt.");
+                                    } else {
+                                        // Handle successful subscription, e.g. logging or incrementing a metric
+                                        logger.info(" Subscribed to the topic: " + topic);
+                                    }
+                                });
+                    });
                 });
     }
 
     private static Mqtt3BlockingClient createAndConnectClient(Broker brokerObj, String identifier) {
-        Mqtt3BlockingClient client = MqttClient.builder()
+        var client = MqttClient.builder()
                 .identifier(identifier)
                 .serverHost(brokerObj.getHostname())
                 .serverPort(brokerObj.getPort())
@@ -55,7 +86,6 @@ public class MqttConnectionService {
                 .build()
                 .toBlocking();
         logger.info("Connecting to {}:{} as ID: {} ", brokerObj.getHostname(), brokerObj.getPort(), identifier);
-        //TODO handle timeout
         client.connect();
         return client;
     }
